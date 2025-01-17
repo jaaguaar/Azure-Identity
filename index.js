e00074ecf650879707c2175118352836c1b0f42f
Vasile <vasile.bubuioc@tech-titans.md>
2025-01-17 13:09:43 +0200

    Return to DefaultAzureCredential

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import { BlobServiceClient } from "@azure/storage-blob";
 (async () => {
   try {
     // Отримання ідентифікатора User-Assigned Managed Identity з змінної середовища
-    const clientId = process.env.AZURE_CLIENT_ID;
-    if (!clientId) {
-      throw new Error(
-        "AZURE_CLIENT_ID is not set. Please set the environment variable with your Managed Identity Client ID."
-      );
-    }
+    // const clientId = process.env.AZURE_CLIENT_ID;
+    // if (!clientId) {
+    //   throw new Error(
+    //     "AZURE_CLIENT_ID is not set. Please set the environment variable with your Managed Identity Client ID."
+    //   );
+    // }
 
     // Отримання токена доступу за допомогою User-Assigned Managed Identity
     const credential = new DefaultAzureCredential();
